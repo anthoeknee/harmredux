2008ce3d15b07e640e76dc62009edaa22be522c6
anthoeknee <anthoeknee@proton.me>
2023-01-19 00:16:03 +0000

    first

---
+++ data/headerNavLinks.js
@@ -0,0 +1,23 @@
+const headerNavLinks = [
+  {
+    href: '/links',
+    title: 'Links',
+  },
+  {
+    href: '/guides',
+    title: 'Guides',
+  },
+  {
+    href: '/about',
+    title: 'About Us',
+  },
+  {
+    href: '/contrib',
+    title: 'Contribute',
+  },
+  {
+    href: '/donate',
+    title: 'Donate',
+  },
+]
+export default headerNavLinks
